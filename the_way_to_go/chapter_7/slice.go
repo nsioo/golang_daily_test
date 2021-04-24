b8a47f03ac3af8ebfd7024d20bdd0f9970901fac
gdis5251 <841784596@qq.com>
2021-04-24 23:01:25 +0800

    深度优先搜索

--- the_way_to_go/chapter_7/slice.go
+++ the_way_to_go/chapter_7/slice.go
@@ -1,1 +1,20 @@
-package chapter_7
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().Unix())
+	for {
+		tmp := rand.Intn(2)
+		fmt.Println(tmp)
+		if tmp == 1{
+			break
+		}
+
+	}
+}
