4ea0269b2b2e054d1d1de7d47fe8a839e39cd6b2
gdis5251 <841784596@qq.com>
2021-04-24 23:47:42 +0800

    format

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import (
 	"fmt"
 )
 
-func main()  {
-	fmt.Println(leet_code.OpenLock([]string{"0201","0101","0102","1212","2002"}, "0202"))
+func main() {
+	fmt.Println(leet_code.OpenLock([]string{"0201", "0101", "0102", "1212", "2002"}, "0202"))
 }
